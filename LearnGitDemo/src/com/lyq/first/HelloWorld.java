ee6fb0c6e3234e3c20296df6355d71d845ee8fb4
liuyq <759189749@qq.com>
2021-01-08 10:08:28 +0800

    提交所有文件

--- LearnGitDemo/src/com/lyq/first/HelloWorld.java
+++ LearnGitDemo/src/com/lyq/first/HelloWorld.java
@@ -6,8 +6,9 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("hello git!");
 		System.out.println("perfect!!");
-		System.out.println("add third");
+		System.out.println("写一写中文吧");
 		System.out.println("oh my god");
+		System.out.println("add aaaaa");
 	}
 
 }
